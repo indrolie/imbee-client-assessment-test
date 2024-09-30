2f48335b8a5252f5b79737631609f8418e89f70c
Indro Lie <indro@vouch.sg>
2024-09-30 16:09:13 +0700

    [fix] wrong file directory declaration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import Home from '../Home'
+import Home from './components/Home'
 import './index.css'
 
 class App extends Component {
